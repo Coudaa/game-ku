16581d69677a236faefba9c4f4fba7e32debcbe9
Alfonsus Jericho <alfonsusjericho31@gmail.com>
2021-10-29 14:41:46 +0700

    UTS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TournamentComponent } from './tournament/tournament.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Profile', component: ProfileComponent},
+  {path: 'Tournament', component: TournamentComponent},
+  {path: 'Leaderboard', component: LeaderboardComponent},
+  {path: 'Home', component: HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [ProfileComponent, TournamentComponent, LeaderboardComponent, HomeComponent]
